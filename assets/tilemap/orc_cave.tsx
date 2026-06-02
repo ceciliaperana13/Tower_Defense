3e85fa62243e317e5bfbc1d976160d392033d9c3
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-02 13:29:57 +0200

    feat : create and add tilemap for the game

---
+++ assets/tilemap/orc_cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="orc_cave" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../sprites/big_buildings/orc_cave.png" width="64" height="64"/>
+</tileset>
