6693d2c8d932cc62db6298f0aa84ceec14c664c3
ceciliaperana13 <cperana13@gmail.com>
2026-06-02 10:37:55 +0200

    GameController en cours

--- script/source/Controller/GameController.cpp
+++ script/source/Controller/GameController.cpp
@@ -1,12 +1,41 @@
-// GameController complet avec Map, GameView, SoundManager, Timer, GameState a gerer en fxml3
-#include "GameController.hpp"
-#include "SoundManager.hpp"
-#include <iostream>
+        // // GameController complet avec Map, GameView, SoundManager, Timer, GameState a gerer en fxml3
+        // #include "GameController.hpp"
+        // #include "SoundManager.hpp"
+        // #include <iostream>
 
 
-//---Constructeur
-GameController::GameController(sf::RenderWindow& window)
-    : m_window(window)
-{
-    // TODO : initialiser la map, la vue, le timer, le SoundManager, etc.
-}
+        // //---Constructeur
+        // GameController::GameController(sf::RenderWindow& window)
+        //     : m_window(window)
+        // {
+        //     // TODO : initialiser la map, la vue, le timer, le SoundManager, etc.
+        // }
+        // //---run
+        // void GameController::run(){
+        //     SoundManager::getInstance().playMusic("main_theme");
+
+        //     sf::Clock clock;
+        //     while (m_running && m_window.isOpen()) {
+        //         sf::Time dt = clock.restart();
+
+        //         // TODO : gérer les événements, mettre à jour la logique du jeu, et rendre la vue
+        //         // Exemple de gestion d'événement basique :
+        //         sf::Event event;
+        //         while (m_window.pollEvent(event)) {
+        //             if (event.type == sf::Event::Closed) {
+        //                 m_window.close();
+        //             }
+        //             // TODO : gérer les autres événements (input, etc.)
+        //         }
+
+        //         // TODO : mettre à jour la logique du jeu avec dt.asSeconds()
+
+        //         m_window.clear();
+        //         // TODO : dessiner la vue du jeu
+        //         m_window.display();
+        //     }
+        //     // TODO : faire les nettoyages nécessaires (sauvegarder la partie, etc.)
+        //     SoundManager::getInstance().stopMusic();
+        //     // Todo : éventuellement jouer un son de fin de partie, afficher un écran de Game Over, etc.
+
+        // }
