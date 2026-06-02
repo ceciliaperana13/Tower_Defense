8c3563c58395106a7f3e9d911c79de9024f41b7f
ceciliaperana13 <cperana13@gmail.com>
2026-06-02 13:23:54 +0200

    en cours

--- script/include/Controller/GameController.hpp
+++ script/include/Controller/GameController.hpp
@@ -1,66 +1,15 @@
 #pragma once
-#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-// #include "Map.hpp"
-// #include "GameView.hpp"
-// #include "SaveController.hpp"
-// #include "SoundManager.hpp"
-// #include "Timer.hpp"
-// #include "GameState.hpp"
+class GameController {
+public:
+    GameController(sf::RenderWindow& window);
 
-// class GameController {
-// public:
-//     GameController(sf::RenderWindow& win)
-//         : window(win), gameState(GameState::Playing) {}
+    void startGame();
+    void update(float dt);
+    void render();
+    void onInput(const sf::Event& e);
 
-//     void startGame() {
-//         map.load();
-//         timer.reset();
-//         sound.playMusic("main_theme");
-//     }
-
-//     void update(float dt) {
-//         if (gameState != GameState::Playing)
-//             return;
-
-//         map.update(dt);
-
-//         spawnWave();
-//         checkWinLoss();
-//     }
-
-//     void onInput(const sf::Event& e) {
-//         if (e.is<sf::Event::Closed>())
-//             window.close();
-
-//         if (auto* key = e.getIf<sf::Event::KeyPressed>()) {
-//             if (key->scancode == sf::Keyboard::Scan::Escape)
-//                 window.close();
-//         }
-//     }
-
-//     void draw() {
-//         view.apply(window);
-//         map.draw(window);
-//     }
-
-// private:
-//     void spawnWave() {
-//         // logique d'apparition d’ennemis
-//     }
-
-//     void checkWinLoss() {
-//         // conditions de victoire/défaite
-//     }
-
-// private:
-//     sf::RenderWindow& window;
-
-//     Map map;
-//     GameView view;
-//     SaveController save;
-//     SoundManager sound;
-//     Timer timer;
-//     GameState gameState;
-// };
+private:
+    sf::RenderWindow& m_window;
+};
--- script/source/Controller/GameController.cpp
+++ script/source/Controller/GameController.cpp
@@ -1,41 +1,23 @@
-        // // GameController complet avec Map, GameView, SoundManager, Timer, GameState a gerer en fxml3
-        // #include "GameController.hpp"
-        // #include "SoundManager.hpp"
-        // #include <iostream>
+#include "GameController.hpp"
+#include <iostream>
 
+GameController::GameController(sf::RenderWindow& window)
+    : m_window(window)
+{
+}
 
-        // //---Constructeur
-        // GameController::GameController(sf::RenderWindow& window)
-        //     : m_window(window)
-        // {
-        //     // TODO : initialiser la map, la vue, le timer, le SoundManager, etc.
-        // }
-        // //---run
-        // void GameController::run(){
-        //     SoundManager::getInstance().playMusic("main_theme");
+void GameController::startGame() {
+    std::cout << "Game started\n";
+}
 
-        //     sf::Clock clock;
-        //     while (m_running && m_window.isOpen()) {
-        //         sf::Time dt = clock.restart();
+void GameController::update(float dt) {
+    // vide pour l'instant
+}
 
-        //         // TODO : gérer les événements, mettre à jour la logique du jeu, et rendre la vue
-        //         // Exemple de gestion d'événement basique :
-        //         sf::Event event;
-        //         while (m_window.pollEvent(event)) {
-        //             if (event.type == sf::Event::Closed) {
-        //                 m_window.close();
-        //             }
-        //             // TODO : gérer les autres événements (input, etc.)
-        //         }
+void GameController::render() {
+    // vide pour l'instant
+}
 
-        //         // TODO : mettre à jour la logique du jeu avec dt.asSeconds()
-
-        //         m_window.clear();
-        //         // TODO : dessiner la vue du jeu
-        //         m_window.display();
-        //     }
-        //     // TODO : faire les nettoyages nécessaires (sauvegarder la partie, etc.)
-        //     SoundManager::getInstance().stopMusic();
-        //     // Todo : éventuellement jouer un son de fin de partie, afficher un écran de Game Over, etc.
-
-        // }
+void GameController::onInput(const sf::Event& e) {
+    // vide pour l'instant
+}
