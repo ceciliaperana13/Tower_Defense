72d4684378979ffa51229ba15dac333a5672b742
Yannis-Sandoval <yannis.sandoval@laplateforme.io>
2026-06-03 15:35:43 +0200

    add Tower.hpp Tower.cpp update tower_values.json

--- script/include/Model/Tower.hpp
+++ script/include/Model/Tower.hpp
@@ -2,54 +2,64 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
 #include <string>
+#include "Enemy.hpp"
 
 class Tower {
 public:
     // Constructeur direct
-    Tower(int id,
-		int damage,
-		int range,
-		int fireRate,
-		int nbTarget,
-        const std::string& sprite1Path,
-        const std::string& sprite2Path);
+    Tower(int id, int damage, int range, int fireRate, int nbTarget,
+        int damagePerSecond, int slowness, int effectDuration, int aoeRadius, int level,
+        const std::string& spriteBuildingPath,
+        const std::string& spriteProjectilePath,
+        const std::vector<sf::Vector2f>& waypoints);
 
     // Constructeur depuis un fichier JSON
-    static Tower fromJson(const std::string& jsonPath, const std::string& type);
+    static Tower fromJson(const std::string& jsonPath, const std::string& type, const std::vector<sf::Vector2f>& waypoints);
 
+    void upgrade();
     void targeting();
-    void attack();
+    virtual void attack();
 
     void render(sf::RenderWindow& window);
 
     void setPosition(float x, float y);
 
-    int getId()                                      const {return id;}
-    void setId(int newId)                            {id = newId;}
-    int getDamage ()                                 const {return damage;}
-    void setDamage (int newDamage)                   {damage = newDamage;}
-    int getRange ()                                  const {return range;}
-    void setRange (int newRange)                     {range = newRange;} 
-    int getFireRate ()                               const {return fireRate;}
-    void setFireRate (int newFireRate)               {fireRate = newFireRate;}
-    int getNbTarget ()                               const {return nbTarget;}
-    void setNbTarget (int newNbTarget)               {nbTarget = newNbTarget;}
-    int getLevel ()                                  const {return level;}
+    int  getId()                                      const {return m_id;}
+    void setId(int newId)                            {m_id = newId;}
+    int  getDamage ()                                 const {return m_damage;}
+    void setDamage (int newDamage)                   {m_damage = newDamage;}
+    int  getRange ()                                  const {return m_range;}
+    void setRange (int newRange)                     {m_range = newRange;} 
+    int  getFireRate ()                               const {return m_fireRate;}
+    void setFireRate (int newFireRate)               {m_fireRate = newFireRate;}
+    int  getNbTarget ()                               const {return m_nbTarget;}
+    void setNbTarget (int newNbTarget)               {m_nbTarget = newNbTarget;}
+    int  getDamagePerSecond ()                        const {return m_damagePerSecond;}
+    void setDamagePerSecond (int newDamagePerSecond) {m_damagePerSecond = newDamagePerSecond;}
+    int  getSlowness ()                               const {return m_slowness;}
+    void setSlowness (int newSlowness)               {m_slowness = newSlowness;}
+    int  getEffectDuration ()                         const {return m_effectDuration;}
+    void setEffectDuration (int newEffectDuration)   {m_effectDuration = newEffectDuration;}
+    int  getAoeRadius ()                              const {return m_aoeRadius;}
+    void setAoeRadius (int newAoeRadius)             {m_aoeRadius = newAoeRadius;}
+    int  getLevel ()                                  const {return m_level;}
 
 private:
-    int id;
-    int damage;
-    int range;
-    int fireRate;
-    int damagePerSecond;
-    int slowness;
-    int effectDuration;
-    int aoeRadius;
-    int nbTarget;
-    int targets;
-    int level;
+    int m_id;
+    int m_damage;
+    int m_range;
+    int m_fireRate;
+    int m_nbTarget;
+    int m_damagePerSecond;
+    int m_slowness;
+    int m_effectDuration;
+    int m_aoeRadius;
+    int m_level;
+    Enemy m_targets;
 
-    sf::Texture               texture1;
-    sf::Texture               texture2;
-    std::optional<sf::Sprite> sprite;
+    sf::Texture               m_tex1;
+    sf::Texture               m_tex2;
+    std::optional<sf::Sprite> m_sprite;
+
+    std::vector<sf::Vector2f> m_waypoints;
 };
